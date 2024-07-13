3bafd1fdfcb6c44dbee57ec4655b4945d451f041
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-07-13 02:48:23 +0000

    Best currency input ever

--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -1,5 +1,6 @@
-import { InputHTMLAttributes, ReactNode } from "react";
+import { InputHTMLAttributes, ReactNode, useState } from "react";
 import { Slot } from "@radix-ui/react-slot";
+import { toCurrency } from "../../utils/toCurrency";
 
 export interface TextInputInputProps
   extends InputHTMLAttributes<HTMLInputElement> {}
@@ -47,8 +48,66 @@ function TextInputInput(props: TextInputInputProps) {
 }
 TextInputInput.displayName = "TextInput.Input";
 
+interface TextInputCurrencyProps {
+  defaultValue: number;
+  onChange: (value: number) => void;
+  currency?: string;
+  locale?: string;
+}
+function TextInputCurrency({
+  defaultValue,
+  onChange,
+  locale,
+  currency,
+}: TextInputCurrencyProps) {
+  const [displayValue, setDisplayValue] = useState(
+    defaultValue?.toFixed(2).replace(/\D/g, "") || ""
+  );
+  const numericValue = Number(displayValue) / 100;
+
+  const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+    const numbers = e.clipboardData.getData("text/plain").replace(/\D/g, "");
+    if (numbers.trim()) setDisplayValue(numbers);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Delete" || e.key === "Backspace") {
+      const hasSelection =
+        e.currentTarget.selectionStart !== e.currentTarget.selectionEnd;
+      if (hasSelection) {
+        setDisplayValue("");
+        return;
+      }
+    }
+    if (e.key === "Backspace") {
+      const value = displayValue
+        .split("")
+        .filter((_, i) => i < displayValue.length - 1)
+        .join("");
+      setDisplayValue(value);
+    }
+    if ("0123456789".includes(e.key)) {
+      setDisplayValue(displayValue + e.key);
+    }
+  };
+
+  return (
+    <input
+      className="outline-0 bg-transparent flex-1 text-zinc-900 text-sm placeholder:text-slate-500"
+      inputMode="numeric"
+      type="text"
+      onChange={() => onChange(numericValue)}
+      value={toCurrency(numericValue, { locale, currency })}
+      onPaste={handlePaste}
+      onKeyDown={handleKeyDown}
+    />
+  );
+}
+TextInputCurrency.displayName = "TextInput.CurrencyInput";
+
 export const TextInput = {
   Root: TextInputRoot,
   Input: TextInputInput,
   Icon: TextInputIcon,
+  CurrencyInput: TextInputCurrency,
 };
