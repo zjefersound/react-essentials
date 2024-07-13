f692052e1f27687b0ebfc055bcd69b4c063dcbc7
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-07-13 02:47:42 +0000

    setup vite port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    hmr: {
+      clientPort: 3000,
+    },
+  },
   plugins: [react()],
 })
