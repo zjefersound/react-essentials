5fb8d2f9a7eb75fa5154641abf179ffcb4ea216b
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-07-16 00:09:20 +0000

    Add select component

---
+++ src/components/form/Select.stories.tsx
@@ -0,0 +1,90 @@
+import { Select, SelectRootProps } from "./Select";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Form/Select",
+  component: Select.Root,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    placeholder: "Select the state",
+    defaultValue: "",
+    required: true,
+    onChange: (value) => {
+      console.log(value);
+    },
+    children: [
+      <Select.Item value="SC">Santa Catarina</Select.Item>,
+      <Select.Item value="SP">São Paulo</Select.Item>,
+      <Select.Item value="PR">Paraná</Select.Item>,
+    ],
+  },
+  argTypes: {
+    placeholder: {
+      control: {
+        type: "text",
+      },
+    },
+    defaultValue: {
+      control: {
+        type: "text",
+      },
+    },
+    value: {
+      control: {
+        type: "text",
+      },
+    },
+    required: {
+      control: { type: "boolean" },
+    },
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<SelectRootProps>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+export const Custom: Story = {
+  args: {
+    children: [
+      <Select.Item value="1">
+        <div className="flex items-center">
+          <img
+            alt="Photo"
+            className="h-7 w-7 object-cover mr-2 rounded-full"
+            src="https://picsum.photos/300/300?random=1"
+          />
+          Alex Smith
+        </div>
+      </Select.Item>,
+      <Select.Item value="2">
+        <div className="flex items-center">
+          <img
+            alt="Photo"
+            className="h-7 w-7 object-cover mr-2 rounded-full"
+            src="https://picsum.photos/300/300?random=2"
+          />
+          Taylor Brown
+        </div>
+      </Select.Item>,
+      <Select.Item value="3" disabled>
+        <div className="flex items-center">
+          <img
+            alt="Photo"
+            className="h-7 w-7 object-cover mr-2 rounded-full"
+            src="https://picsum.photos/300/300?random=3"
+          />
+          Sam Hallway
+        </div>
+      </Select.Item>,
+    ],
+  },
+};
---
+++ src/components/form/Select.tsx
@@ -0,0 +1,93 @@
+import React, { LegacyRef, ReactNode } from "react";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import clsx from "clsx";
+import { RxCheck, RxChevronDown, RxChevronUp } from "react-icons/rx";
+
+export interface SelectRootProps {
+  children: ReactNode;
+  placeholder?: string;
+  defaultValue?: string;
+  value: string;
+  onChange: (value: string) => void;
+  required?: boolean;
+}
+
+const SelectRoot = ({
+  value,
+  defaultValue,
+  children,
+  placeholder,
+  onChange,
+  required,
+}: SelectRootProps) => (
+  <SelectPrimitive.Root
+    defaultValue={defaultValue}
+    value={value}
+    onValueChange={(value) => onChange(value === null ? "" : value)}
+    required={required}
+  >
+    <SelectPrimitive.Trigger
+      className="h-10
+      flex items-center space-x-2
+      py-4 px-3 rounded-md
+      border 
+      border-slate-300
+      bg-white
+      w-full 
+      outline-0
+      focus-within:ring-2 ring-slate-500  
+      text-sm
+      leading-none text-zinc-900 hover:bg-white  focus:shadow-zinc-900 data-[placeholder]:text-slate-500 outline-none"
+    >
+      <SelectPrimitive.Value placeholder={placeholder} />
+      <SelectPrimitive.Icon className="text-slate-500">
+        <RxChevronDown />
+      </SelectPrimitive.Icon>
+    </SelectPrimitive.Trigger>
+    <SelectPrimitive.Portal>
+      <SelectPrimitive.Content className="z-50 overflow-hidden bg-white border rounded shadow-[0px_10px_38px_-10px_rgba(0,_0,_0,_0.4),_0px_8px_16px_-15px_rgba(22,_23,_24,_0.2)]">
+        <SelectPrimitive.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-slate-300 cursor-default">
+          <RxChevronUp />
+        </SelectPrimitive.ScrollUpButton>
+        <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+        <SelectPrimitive.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-slate-300 cursor-default">
+          <RxChevronDown />
+        </SelectPrimitive.ScrollDownButton>
+      </SelectPrimitive.Content>
+    </SelectPrimitive.Portal>
+  </SelectPrimitive.Root>
+);
+SelectRoot.displayName = "Select.Root";
+
+const SelectItem = React.forwardRef(
+  (
+    {
+      children,
+      className,
+      ...props
+    }: SelectPrimitive.SelectItemProps & React.RefAttributes<HTMLDivElement>,
+    forwardedRef
+  ) => {
+    return (
+      <SelectPrimitive.Item
+        className={clsx(
+          "text-[13px] cursor-pointer leading-none text-slate-500 rounded-md flex items-center h-10 pr-9 pl-6 relative select-none data-[disabled]:text-slate-300 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-slate-100 data-[highlighted]:text-zinc-900",
+          className
+        )}
+        {...props}
+        ref={forwardedRef as LegacyRef<HTMLDivElement>}
+      >
+        <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+        <SelectPrimitive.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+          <RxCheck />
+        </SelectPrimitive.ItemIndicator>
+      </SelectPrimitive.Item>
+    );
+  }
+);
+SelectItem.displayName = "Select.Item";
+
+export const Select = {
+  Root: SelectRoot,
+  Item: SelectItem,
+};
